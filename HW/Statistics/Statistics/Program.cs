19211d0fdc76d9eb0435ef9002aba49f49735007
Stefanishin Mikita <stefanishin92@gmail.com>
2020-11-01 11:19:31 +0200

    add hw_lab7_8

---
+++ HW/Statistics/Statistics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+/*Программа «Статистика».
+Подсчитать, сколько раз каждое слово встречается в заданном тексте. 
+Результат записать в коллекцию Dictionary.
+Пример текста для проверки: 
+Вот дом, Который построил Джек. А это пшеница,
+Которая в темном чулане хранится В доме, 
+Который построил Джек. А это веселая птица-синица,
+Которая часто ворует пшеницу, Которая в темном чулане хранится В доме,
+Который построил Джек.
+*/
+namespace Statistics
+{
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                try
+                {
+                    int x = 5;
+                    int y = x / 0;
+                    Console.WriteLine($"Результат: {y}");
+                }
+                catch
+                {
+                    Console.WriteLine("Возникло исключение!");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally");
+                }
+                Console.WriteLine("Конец программы");
+                Console.Read();
+            }
+        }
+    }
